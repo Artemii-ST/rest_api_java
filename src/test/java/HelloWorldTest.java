0e4cb994aaef784aa6be072dbee88cd127b69184
Iagodin Artem <artem.iagodin@aton.ru>
2022-12-07 22:12:06 +0300

    Commit for home_work

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,18 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class HelloWorldTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Artem");
+    }
+
     @Test
     public void testHelloWorld() {
-        System.out.println("Hello world");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
