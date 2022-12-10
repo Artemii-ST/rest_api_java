e22fc11b1d1ebbc84dbb67f239fee88a5df45908
Iagodin Artem <artem.iagodin@aton.ru>
2022-12-10 16:43:10 +0300

    Added HomeWork_Ex6

---
+++ src/test/java/HomeWork/HomeWorkEx6.java
@@ -0,0 +1,27 @@
+package HomeWork;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HomeWorkEx6 {
+    @Test
+    public void testRestAssured() {
+        String URL = "https://playground.learnqa.ru/api/long_redirect";
+        while (checkRedirectsAndReturnUrl(URL) != null) {
+            URL = checkRedirectsAndReturnUrl(URL);
+            System.out.println(URL);
+        }
+    }
+    public String checkRedirectsAndReturnUrl(String url) {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .get(url)
+                .andReturn();
+
+        String getHeaderLocation = response.getHeader("Location");
+        return getHeaderLocation;
+    }
+}
